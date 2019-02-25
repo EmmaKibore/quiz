e22dfdfcea2bdcf6b98e3ea02eddf7bf2c7bb453
EmmaKibore <emmakibore@gmail.com>
2019-02-25 12:29:13 +0300

    added structural changes to js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
-$("Quiz#Quiz").click(function(){
-    $("#output").click(function(event)
-      event.preventDefault();
-      var qtn1 = parseInt($("input:radio[name=qtn1]:checked").val());
-      var qtn2 = parseInt($("input:radio[name=qtn2]:checked").val());
-      var qtn3 = parseInt($("input:radio[name=qtn3]:checked").val());
-      var qtn4 = parseInt($("input:radio[name=qtn4]:checked").val());
-      var qtn5 = parseInt($("input:radio[name=qtn5]:checked").val());
-      $("#display").text(q1+q2+q3+q4+q5);
+$("#Quiz").submit(function(event){
+     event.preventDefault();
+      var qtn1 = parseInt($("input:radio[name=1]:checked").val());
+      var qtn2 = parseInt($("input:radio[name=2]:checked").val());
+      var qtn3 = parseInt($("input:radio[name=3]:checked").val());
+      var qtn4 = parseInt($("input:radio[name=4]:checked").val());
+      var qtn5 = parseInt($("input:radio[name=5]:checked").val());
 
-function getResult (sum) {
-  if (sum < 50) {
-    result = you have failed
-    return result
-  }else if (50 < && sum < 80) {
-    result = you have perfomed fairly
-  } else {80}
+    var total = qtn1 + qtn2 + qtn3 +qtn4 + qtn5 ;
+    var result = '';
+
+function getTotal(sum) {
+  if (sum <= 49) {
+    alert("you have failed please retake the test")
+
+  }else if (sum >=50  && sum < 80) {
+    alert("average")
+
+  } else  {
+    alert("Excellent")
+  }
+
+  }
+
+var gettotal = total + getTotal(total);
+document.getElementById("output").innerHTML = total;
+});
+});
